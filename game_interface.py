954bbcd780d68b46b53c237a82559e952bf3ae2d
theo-baccam <theo.baccam@laplateforme.io>
2023-12-11 14:50:12 +0100

    interface graphique pendu

---
+++ game_interface.py
@@ -0,0 +1,95 @@
+import pygame
+from game_logic import *
+
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+running = True
+
+
+verif = verify_input
+game = game_state
+
+
+word_list = word_list_loader()
+
+random_word = random_word_select(word_list)
+play_field = "_" * len(random_word)
+
+count = 0
+used_letters = []
+
+background = (32, 32, 64)
+foreground = (0, 192, 255)
+
+font_path = "./NotoSansMono-Regular.ttf"
+
+main_font_size = 20
+main_font = pygame.font.Font(font_path, main_font_size)
+
+used_letters_font_size = 15
+used_letters_font = pygame.font.Font(font_path, used_letters_font_size)
+
+
+while running:
+    lose_surface = main_font.render("Perdu!", True, foreground)
+    lose_center = lose_surface.get_width() / 2
+    lose_x, lose_y = 320 - lose_center, 30
+
+    win_surface = main_font.render("Gagné!", True, foreground)
+    win_center = win_surface.get_width() / 2
+    win_x, win_y = 320 - win_center, 30
+
+    solution_surface = main_font.render(random_word, True, foreground)
+    solution_center = solution_surface.get_width() / 2
+    solution_x, solution_y = 320 - solution_center, 420
+
+    used_letters_surface = used_letters_font.render(" ".join(used_letters), True, foreground)
+    used_letters_center = used_letters_surface.get_width() / 2
+    used_letters_x, used_letters_y = 320 - used_letters_center, 450
+
+    count_surface = main_font.render(f"{count}/7", True, foreground)
+    count_x, count_y = 550, 30
+
+    word_surface = main_font.render(play_field, True, foreground)
+    word_center = word_surface.get_width() / 2
+    word_x, word_y = 320 - word_center, 420
+
+    screen.fill(background)
+
+    if game.lose_state(count):
+        screen.blit(lose_surface, (lose_x, lose_y))
+        screen.blit(solution_surface, (solution_x, solution_y))
+        screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
+        screen.blit(count_surface, (count_x, count_y))
+        pygame.display.flip()
+        pygame.time.delay(1000)
+        running = False
+    elif game.win_state(play_field, random_word):
+        screen.blit(win_surface, (win_x, win_y))
+        screen.blit(solution_surface, (solution_x, solution_y))
+        screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
+        screen.blit(count_surface, (count_x, count_y))
+        pygame.display.flip()
+        pygame.time.delay(1000)
+        running = False
+
+    screen.blit(word_surface, (word_x, word_y))
+    screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
+    screen.blit(count_surface, (count_x, count_y))
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key >= pygame.K_a and event.key <= pygame.K_z:
+                letter = chr(event.key).lower()
+                if verif.was_used(letter, used_letters):
+                    continue
+                add_to_used_letters(letter, used_letters)
+                if not is_in_word(letter, random_word):
+                    count += 1
+                    continue
+                play_field = modify_play_field(letter, play_field, random_word)
+
+pygame.quit()
--- game_logic.py
+++ game_logic.py
@@ -30,7 +30,7 @@ class verify_input:
             return True
 
 
-    def was_used(letter):
+    def was_used(letter, used_letters):
         if letter in used_letters:
             return True
 
@@ -68,49 +68,3 @@ class game_state:
     def win_state(play_field, random_word):
         if play_field == random_word:
             return True
-
-
-verif = verify_input
-game = game_state
-
-word_list = word_list_loader()
-
-random_word = random_word_select(word_list)
-play_field = "_" * len(random_word)
-
-count = 0
-used_letters = []
-
-while True:
-    try:
-        current_state = game.play_state(used_letters, count, play_field)
-        print(current_state)
-
-        letter = player_prompt(used_letters)
-
-        if not verif.is_letter(letter):
-            print(f"'{letter} n'est pas une lettre.'\n")
-            continue
-        elif not verif.is_one_char(letter):
-            print(f"'{letter}' n'est pas valide, il faut que ça soit une lettre.\n")
-            continue
-        elif verif.was_used(letter):
-            print(f"'{letter}' a déjà été utilisé.\n")
-            continue
-
-        convert_to_lowercase(letter)
-        add_to_used_letters(letter, used_letters)
-
-        if not is_in_word(letter, random_word):
-            count += 1
-
-        play_field = modify_play_field(letter, play_field, random_word)
-
-        if game.lose_state(count):
-            print(f"Perdu!\nLa solution était '{random_word}'!\n")
-            break
-        elif game.win_state(play_field, random_word):
-            print(f"Gagné!\nLe mot est bien '{random_word}'\n")
-            break
-    except KeyboardInterrupt:
-        break
