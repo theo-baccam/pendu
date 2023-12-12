b7311a64df7a06bd6bbb49cf06df7c99de3d7ec6
theo-baccam <theo.baccam@laplateforme.io>
2023-12-12 11:50:17 +0100

    réarranger variables, changé noms, constantes

--- game_interface.py
+++ game_interface.py
@@ -1,66 +1,70 @@
-import pygame
 from game_logic import *
-
-pygame.init()
-pygame.font.init()
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-running = True
+import pygame
 
 word = WordHandler
 verif = VerifyInput
 lett = LetterHandler
 game = GameState
 
-play_field = "_" * len(word.random_word)
+pygame.init()
 
-count = 0
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-background = (32, 32, 64)
-foreground = (0, 192, 255)
+clock = pygame.time.Clock()
+running = True
+
+pygame.font.init()
 
 font_path = "./NotoSansMono-Regular.ttf"
 
-main_font_size = 20
-main_font = pygame.font.Font(font_path, main_font_size)
+MAIN_FONT_SIZE = 20
+MAIN_FONT = pygame.font.Font(font_path, MAIN_FONT_SIZE)
 
-used_letters_font_size = 15
-used_letters_font = pygame.font.Font(font_path, used_letters_font_size)
+USED_LETTERS_FONT_SIZE = 15
+USED_LETTERS_FONT = pygame.font.Font(font_path, USED_LETTERS_FONT_SIZE)
 
+BACKGROUND = (32, 32, 64)
+FOREGROUND = (0, 192, 255)
+
+play_field = "_" * len(word.random_word)
+
+life_count = 0
 
 while running:
-    lose_surface = main_font.render("Perdu!", True, foreground)
+    lose_surface = MAIN_FONT.render("Perdu!", True, FOREGROUND)
     lose_center = lose_surface.get_width() / 2
     lose_x, lose_y = 320 - lose_center, 30
 
-    win_surface = main_font.render("Gagné!", True, foreground)
+    win_surface = MAIN_FONT.render("Gagné!", True, FOREGROUND)
     win_center = win_surface.get_width() / 2
     win_x, win_y = 320 - win_center, 30
 
-    solution_surface = main_font.render(word.random_word, True, foreground)
+    solution_surface = MAIN_FONT.render(word.random_word, True, FOREGROUND)
     solution_center = solution_surface.get_width() / 2
     solution_x, solution_y = 320 - solution_center, 420
 
-    used_letters_surface = used_letters_font.render(
-        " ".join(lett.used_letters), True, foreground
+    used_letters_surface = USED_LETTERS_FONT.render(
+        " ".join(lett.used_letters), True, FOREGROUND
     )
     used_letters_center = used_letters_surface.get_width() / 2
     used_letters_x, used_letters_y = 320 - used_letters_center, 450
 
-    count_surface = main_font.render(f"{count}/7", True, foreground)
-    count_x, count_y = 550, 30
+    life_count_surface = MAIN_FONT.render(f"{life_count}/7", True, FOREGROUND)
+    life_count_x, life_count_y = 550, 30
 
-    word_surface = main_font.render(play_field, True, foreground)
+    word_surface = MAIN_FONT.render(play_field, True, FOREGROUND)
     word_center = word_surface.get_width() / 2
     word_x, word_y = 320 - word_center, 420
 
-    screen.fill(background)
+    screen.fill(BACKGROUND)
 
-    if game.lose_state(count):
+    if game.lose_state(life_count):
         screen.blit(lose_surface, (lose_x, lose_y))
         screen.blit(solution_surface, (solution_x, solution_y))
         screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
-        screen.blit(count_surface, (count_x, count_y))
+        screen.blit(life_count_surface, (life_count_x, life_count_y))
         pygame.display.flip()
         pygame.time.delay(1000)
         running = False
@@ -68,14 +72,14 @@ while running:
         screen.blit(win_surface, (win_x, win_y))
         screen.blit(solution_surface, (solution_x, solution_y))
         screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
-        screen.blit(count_surface, (count_x, count_y))
+        screen.blit(life_count_surface, (life_count_x, life_count_y))
         pygame.display.flip()
         pygame.time.delay(1000)
         running = False
 
     screen.blit(word_surface, (word_x, word_y))
     screen.blit(used_letters_surface, (used_letters_x, used_letters_y))
-    screen.blit(count_surface, (count_x, count_y))
+    screen.blit(life_count_surface, (life_count_x, life_count_y))
 
     pygame.display.flip()
     for event in pygame.event.get():
@@ -86,7 +90,7 @@ while running:
                     continue
                 lett.add_to_used_letters(letter)
                 if not verif.is_in_word(letter, word.random_word):
-                    count += 1
+                    life_count += 1
                     continue
                 play_field = modify_play_field(letter, play_field, word.random_word)
 
